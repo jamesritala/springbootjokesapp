d4cb8f1e8ed95011e1d86f3c721ff0784a832c36
James <jamesritala@gmail.com>
2018-11-21 11:44:37 +0200

    Added controller and thymleaf page for joke

---
+++ src/main/java/guru/springboot/controller/JokesController.java
@@ -0,0 +1,22 @@
+package guru.springboot.controller;
+
+import guru.springboot.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    @Autowired
+    private JokeService jokeService;
+
+    @RequestMapping("/")
+    public String getJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
